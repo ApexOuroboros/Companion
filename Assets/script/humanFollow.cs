626d8e022e07f9d55cb30866f1472a6c81545f29
ApexOuroboros <114581783+ApexOuroboros@users.noreply.github.com>
2023-04-24 15:21:43 +0100

    done. do menu

---
+++ Assets/script/humanFollow.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class humanFollow : MonoBehaviour
+{
+    public Transform Player;
+    public int speed = 4;
+    public int maxDist = 10;
+    public int minDist = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        transform.LookAt(Player);
+
+        if(Vector3.Distance(transform.position, Player.position) <= maxDist)
+        {
+            transform.position += transform.forward * speed * Time.deltaTime;
+
+            if(Vector3.Distance(transform.position, Player.position) <= minDist)
+            {
+                //disable
+            }
+
+        }
+
+    }
+}
